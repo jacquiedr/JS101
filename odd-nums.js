2ca347af5885fe233ad1504980296c2b70a6aca0
Jacquie Des Rosiers <jacquiedesrosiers@gmail.com>
2023-03-15 18:52:48 -0400

    further exploration

--- odd-nums.js
+++ odd-nums.js
@@ -1,3 +1,10 @@
+// first technique
 for (let i = 0; i <= 99; i++) {
     if (i % 2 === 1) console.log(i);
+}
+// second technique
+let i = 1;
+while (i <= 99) {
+    console.log(i);
+    i += 2;
 }
